2549fefcf0d30c1ffd2f8080f765b417e4a58f49
novitskynick <novitskynick@gmail.com>
2023-03-19 14:55:57 +0300

    homework done

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,26 +1,27 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import web.model.Car;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
+
+    private final CarService carService;
+
     @GetMapping
-    public String carsList(ModelMap model) {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("BMW", "101", "black"));
-        cars.add(new Car("Mercedes", "202", "white"));
-        cars.add(new Car("Kia", "303", "grey"));
-        cars.add(new Car("Jeep", "404", "red"));
-        cars.add(new Car("Haval", "505", "blue"));
-        model.addAttribute("cars", cars);
+    public String carsList(@RequestParam(required = false) Long carsCount, ModelMap model) {
+        model.addAttribute("cars", carService.getListOfCars(carsCount));
         return "cars";
+    }
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 }
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,19 +1,19 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CarServiceImp implements CarService {
-    static {
-        fillListOfCars();
-    }
 
-    private static final List<Car> cars = new ArrayList<>();
 
-    public static void fillListOfCars() {
+    private final List<Car> cars;
+
+    public void fillListOfCars() {
         cars.add(new Car("BMW", "101", "black"));
         cars.add(new Car("Mercedes", "202", "white"));
         cars.add(new Car("Kia", "303", "grey"));
@@ -24,7 +24,16 @@ public class CarServiceImp implements CarService {
 
     @Override
     public List<Car> getListOfCars(Long carsCount) {
+        if (carsCount == null) {
+            carsCount = (long) Integer.MAX_VALUE;
+        }
         return cars.stream().limit(carsCount).collect(Collectors.toList());
     }
+
+    public CarServiceImp() {
+        cars = new ArrayList<>();
+        fillListOfCars();
+    }
+
 
 }
